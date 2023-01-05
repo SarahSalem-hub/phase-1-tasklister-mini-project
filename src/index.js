69de64af6ef2772fe53bbe304e249399595328e9
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2023-01-05 17:17:41 +0300

    all Deliverables are done + the first two of the Stretch deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let obj = document.querySelector("#create-task-form")
+  let color ;
+  let optionsIndex;
+
+  obj.addEventListener("submit",function(e){
+    e.preventDefault()
+    addTask(e.target.new_task_description.value, color, optionsIndex);
+    obj.reset()
+  })
+
+
+  let select = document.querySelector("select")
+  select.addEventListener("change",function(){
+   color =  select.options[select.selectedIndex]
+   optionsIndex = select.options.selectedIndex
+  })
+
+  let down = document.querySelector("#down")
+  down.addEventListener("click",order)
+
 });
+
+
+function addTask(input,color,optionsIndex){
+  let list = document.createElement("li")
+  let btn = document.createElement("button")
+    btn.addEventListener("click", deleteTask)
+    btn.innerHTML = "X"
+    list.innerHTML = `${input} `
+    list.setAttribute("value",optionsIndex)
+    list.style.color = color.value
+    list.appendChild(btn)
+    document.querySelector("#tasks").appendChild(list)
+}
+
+function deleteTask(e){
+    e.target.parentNode.remove()
+}
+
+function order(){
+  let tasks = document.querySelector("#tasks")
+  let listValue = [] ;
+    
+   let list = Array.from(tasks.children)
+   list.sort(function(a,b){
+    return  a.getAttribute("value")
+    - b.getAttribute("value")
+   }) 
+
+   list.forEach(function(ele){
+    tasks.appendChild(ele)
+   })
+  
+   
+}
