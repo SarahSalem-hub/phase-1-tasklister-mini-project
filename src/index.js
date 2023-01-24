1badd908b9c2b1cf306e7ff58d8836fdaf3d7b1f
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-01-24 16:38:27 +0300

    order the tasks whenever a new one added

--- src/index.js
+++ src/index.js
@@ -1,56 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let obj = document.querySelector("#create-task-form")
-  let color ;
+  let obj = document.querySelector("#create-task-form");
+  let color;
   let optionsIndex;
 
-  obj.addEventListener("submit",function(e){
-    e.preventDefault()
+  obj.addEventListener("submit", function (e) {
+    e.preventDefault();
     addTask(e.target.new_task_description.value, color, optionsIndex);
-    obj.reset()
-  })
+    obj.reset();
+  });
 
+  let select = document.querySelector("select");
+  select.addEventListener("change", function () {
+    color = select.options[select.selectedIndex];
+    optionsIndex = select.options.selectedIndex;
+  });
 
-  let select = document.querySelector("select")
-  select.addEventListener("change",function(){
-   color =  select.options[select.selectedIndex]
-   optionsIndex = select.options.selectedIndex
-  })
-
-  let down = document.querySelector("#down")
-  down.addEventListener("click",order)
-
+  let down = document.querySelector("#down");
+  down.addEventListener("click", order);
 });
 
-
-function addTask(input,color,optionsIndex){
-  let list = document.createElement("li")
-  let btn = document.createElement("button")
-    btn.addEventListener("click", deleteTask)
-    btn.innerHTML = "X"
-    list.innerHTML = `${input} `
-    list.setAttribute("value",optionsIndex)
-    list.style.color = color.value
-    list.appendChild(btn)
-    document.querySelector("#tasks").appendChild(list)
+function addTask(input, color, optionsIndex) {
+  let list = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", deleteTask);
+  btn.innerHTML = "X";
+  list.innerHTML = `${input} `;
+  list.setAttribute("value", optionsIndex);
+  list.style.color = color.value;
+  list.appendChild(btn);
+  document.querySelector("#tasks").appendChild(list);
+  order();
 }
 
-function deleteTask(e){
-    e.target.parentNode.remove()
+function deleteTask(e) {
+  e.target.parentNode.remove();
 }
 
-function order(){
-  let tasks = document.querySelector("#tasks")
-  let listValue = [] ;
-    
-   let list = Array.from(tasks.children)
-   list.sort(function(a,b){
-    return  a.getAttribute("value")
-    - b.getAttribute("value")
-   }) 
+function order() {
+  let tasks = document.querySelector("#tasks");
 
-   list.forEach(function(ele){
-    tasks.appendChild(ele)
-   })
-  
-   
+  let list = Array.from(tasks.children);
+  list.sort(function (a, b) {
+    return a.getAttribute("value") - b.getAttribute("value");
+  });
+
+  list.forEach(function (ele) {
+    tasks.appendChild(ele);
+  });
 }
